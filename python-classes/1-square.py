11304d3887a6b59cbd3d37b92a0b35fb65e5b09c
Rubabe Qubadova <rubgubadova7@gmail.com>
2026-03-19 12:42:40 +0000

    add task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """A class that defines a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size: The size of the square.
+        """
+        self.__size = size
