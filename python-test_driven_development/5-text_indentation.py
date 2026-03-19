af9de50cae5f3e3df3fc1bb6aa1168dc2a59dbb5
Rubabe Qubadova <rubgubadova7@gmail.com>
2026-03-19 05:51:48 +0000

    add task 4

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module defines the text_indentation function."""
+
+
+def text_indentation(text):
+    """Print text with 2 new lines after each '.', '?' and ':'.
+
+    Args:
+        text (str): The text to print.
+
+    Raises:
+        TypeError: If text is not a string.
+
+    Example:
+        >>> text_indentation("Hello.")
+        Hello.
+        <BLANKLINE>
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in ".?:":
+            print("\n")
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+        i += 1
+    print("")
