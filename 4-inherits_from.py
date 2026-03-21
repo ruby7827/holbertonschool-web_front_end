621af94fa0cdf3da2b0deaa2c11115ed7a66eb91
Rubabe Qubadova <rubgubadova7@gmail.com>
2026-03-21 08:07:50 +0000

    add task 4

--- 4-inherits_from.py
+++ 4-inherits_from.py
@@ -3,6 +3,5 @@
 
 
 def inherits_from(obj, a_class):
-    """Return True if obj is an instance of a class that inherited
-    from a_class."""
+    """Return True if obj inherited from a_class."""
     return isinstance(obj, a_class) and type(obj) is not a_class
