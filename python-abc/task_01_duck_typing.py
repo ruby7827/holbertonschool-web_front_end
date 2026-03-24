c496312061bd01b8067fd43dbded18b6c4f5a72b
Rubabe Qubadova <rubgubadova7@gmail.com>
2026-03-24 18:53:07 +0000

    add task 1

---
+++ python-abc/task_01_duck_typing.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""This module defines Shape, Circle, Rectangle and shape_info."""
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    """An abstract class that defines a shape."""
+
+    @abstractmethod
+    def area(self):
+        """Return the area of the shape."""
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        """Return the perimeter of the shape."""
+        pass
+
+
+class Circle(Shape):
+    """A class that defines a circle."""
+
+    def __init__(self, radius):
+        """Initialize a new Circle.
+
+        Args:
+            radius (float): The radius of the circle.
+        """
+        self.radius = radius
+
+    def area(self):
+        """Return the area of the circle."""
+        return math.pi * self.radius ** 2
+
+    def perimeter(self):
+        """Return the perimeter of the circle."""
+        return 2 * math.pi * self.radius
+
+
+class Rectangle(Shape):
+    """A class that defines a rectangle."""
+
+    def __init__(self, width, height):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (float): The width of the rectangle.
+            height (float): The height of the rectangle.
+        """
+        self.width = width
+        self.height = height
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.width * self.height
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle."""
+        return 2 * (self.width + self.height)
+
+
+def shape_info(shape):
+    """Print the area and perimeter of a shape.
+
+    Args:
+        shape (Shape): The shape to print info about.
+    """
+    print("Area: {}".format(shape.area()))
+    print("Perimeter: {}".format(shape.perimeter()))
